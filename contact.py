439e1dee431ca93ba43773423cc61f246ba10882
AklerQ <akler.mortiferus@gmail.com>
2017-03-05 13:13:30 +0300

    Вынесение свойств контакта в отдельный класс

---
+++ contact.py
@@ -0,0 +1,21 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, companyname, address, homenumber,
+                       worknumber, email1, email2, birth_date, birth_month, birth_year, anniversary_date,
+                       anniversary_month, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.companyname = companyname
+        self.address = address
+        self.homenumber = homenumber
+        self.worknumber = worknumber
+        self.email1 = email1
+        self.email2 = email2
+        self.birth_date = birth_date
+        self.birth_month = birth_month
+        self.birth_year = birth_year
+        self.anniversary_date = anniversary_date
+        self.anniversary_month = anniversary_month
+        self.notes = notes
