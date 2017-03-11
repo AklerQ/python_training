f1e2d902261fbacc93b0615d5d0f5474167cbd21
AklerQ <akler.mortiferus@gmail.com>
2017-03-12 00:47:01 +0300

    Выделен класс-помощник для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов", nickname="Ваня",
                         companyname="Иван и Ко", address="посёлок Иваново", homenumber="543-66-12", worknumber=
                         "+7(456)1-14-15", email1="mail@ivan.io", email2="ivanov@ivan.io",
@@ -21,4 +21,4 @@ def test_add_contact(app):
                         birth_month="//div[@id='content']/form/select[2]//option[8]", birth_year="1987",
                         anniversary_date="//div[@id='content']/form/select[3]//option[20]",
                         anniversary_month="//div[@id='content']/form/select[4]//option[12]", notes="Текст заметки"))
-    app.logout()
+    app.session.logout()
