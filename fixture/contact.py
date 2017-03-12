eb244fae84a96b56aba9495f1745e5640e7fb133
AklerQ <akler.mortiferus@gmail.com>
2017-03-12 23:35:46 +0300

    Добавлен тест с удалением первого контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -58,3 +58,10 @@ class ContactHelper:
         # submit created contact
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.app.navigation.return_to_home_page()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
+        self.app.navigation.return_to_home_page()
