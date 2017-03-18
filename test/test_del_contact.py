52c1d105c32f569a16fb698267361885d3b77993
AklerQ <akler.mortiferus@gmail.com>
2017-03-19 01:43:57 +0300

    Добавлены предусловия для модификации/удаления контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+from model.contact import Contact
 
 
 def test_delete_first_contact(app):
+    if app.contact.count_contacts() == 0:
+        app.contact.create(Contact(firstname="For delete", birth_date="//div[@id='content']/form/select[1]//option[1]",
+                           birth_month="//div[@id='content']/form/select[2]//option[1]",
+                           anniversary_date="//div[@id='content']/form/select[3]//option[1]",
+                           anniversary_month="//div[@id='content']/form/select[4]//option[1]"))
     app.contact.delete_first_contact()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,12 +3,17 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    if app.contact.count_contacts() == 0:
+        app.contact.create(Contact(firstname="For modify", birth_date="//div[@id='content']/form/select[1]//option[1]",
+                           birth_month="//div[@id='content']/form/select[2]//option[1]",
+                           anniversary_date="//div[@id='content']/form/select[3]//option[1]",
+                           anniversary_month="//div[@id='content']/form/select[4]//option[1]"))
     app.contact.edit_first_contact(Contact(firstname="Отредактирован", middlename="Отредактирович",
                                          lastname="Отредактированский", nickname="Редактор",
                                          companyname='ОАО "Редакция и Мир"', address="редакторский городок",
                                          homenumber="567-22-04", worknumber="456", email1="glavred@mir.ur",
                                          notes="Здесь могла бы быть ваша реклама", email2="",
-                                         birth_date="//div[@id='content']/form/select[1]//option[2]",
-                                         birth_month="//div[@id='content']/form/select[2]//option[2]", birth_year="",
-                                         anniversary_date="//div[@id='content']/form/select[3]//option[2]",
-                                         anniversary_month="//div[@id='content']/form/select[4]//option[2]"))
+                                         birth_date="//div[@id='content']/form/select[1]//option[4]",
+                                         birth_month="//div[@id='content']/form/select[2]//option[5]", birth_year="",
+                                         anniversary_date="//div[@id='content']/form/select[3]//option[6]",
+                                         anniversary_month="//div[@id='content']/form/select[4]//option[7]"))
