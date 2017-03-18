b0ed9e7b8a96e005da1d5304265a5c712ca49d7a
AklerQ <akler.mortiferus@gmail.com>
2017-03-18 21:39:14 +0300

    Расширены тесты по модификации групп

--- fixture/group.py
+++ fixture/group.py
@@ -17,8 +17,7 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.app.navigation.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.app.navigation.return_to_groups_page()
@@ -26,8 +25,7 @@ class GroupHelper:
     def edit_first_group(self, group):
         wd = self.app.wd
         self.app.navigation.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # init group edition
         wd.find_element_by_name("edit").click()
         # fill group form
@@ -36,14 +34,18 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.app.navigation.return_to_groups_page()
 
-    def fill_group_fields(self, group):
+    def select_first_group(self):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("selected[]").click()
+
+    def fill_group_fields(self, group):
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="test_group_refactor", header="Logo here", footer="Input your comment"))
+    app.group.create(Group(name="Test group", header="Logo here", footer="Input your comment"))
     app.session.logout()
 
 
