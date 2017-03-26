d63cd54ecc3f931ddd47973a05c448beb801b869
AklerQ <akler.mortiferus@gmail.com>
2017-03-27 01:02:21 +0300

    Добавлена проверка на эквивалентность списков при добавлении групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,21 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="Test group", header="Logo here", footer="Input your comment"))
+    input_group = Group(name="Test group", header="Logo here", footer="Input your comment")
+    app.group.create(input_group)
+    # Test validation
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(input_group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    input_group = Group(name="", header="", footer="")
+    app.group.create(input_group)
+    # Test validation
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(input_group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
