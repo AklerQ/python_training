66a4b4718b1647295eb37c5b1ec3863dc067b144
AklerQ <akler.mortiferus@gmail.com>
2017-03-28 02:10:05 +0300

    Добавлена проверка для тестов по удалению контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,8 +4,15 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count_contacts() == 0:
-        app.contact.create(Contact(firstname="For delete", birth_date="//div[@id='content']/form/select[1]//option[1]",
+        app.contact.create(Contact(firstname="Тест_имени", lastname="Тест_фамилии",
+                           birth_date="//div[@id='content']/form/select[1]//option[1]",
                            birth_month="//div[@id='content']/form/select[2]//option[1]",
                            anniversary_date="//div[@id='content']/form/select[3]//option[1]",
                            anniversary_month="//div[@id='content']/form/select[4]//option[1]"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    # Test validation
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
