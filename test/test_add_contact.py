8216a9f982a8423a4e631331899cc8b26f4dcd72
AklerQ <akler.mortiferus@gmail.com>
2017-03-30 23:28:49 +0300

    Добавлены проверки на эквивалентность для создания и редактирования контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,20 +3,34 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.contact.create(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов", nickname="Ваня",
+    old_contacts = app.contact.get_contact_list()
+    input_contact = Contact(firstname="Иван", middlename="Иванович", lastname="Иванов", nickname="Ваня",
                                companyname="Иван и Ко", address="посёлок Иваново", homenumber="543-66-12", worknumber=
                                "+7(456)1-14-15", email1="mail@ivan.io", email2="ivanov@ivan.io",
                                birth_date="//div[@id='content']/form/select[1]//option[16]",
                                birth_month="//div[@id='content']/form/select[2]//option[8]", birth_year="1987",
                                anniversary_date="//div[@id='content']/form/select[3]//option[20]",
                                anniversary_month="//div[@id='content']/form/select[4]//option[12]",
-                               notes="Текст заметки"))
+                               notes="Текст заметки")
+    app.contact.create(input_contact)
+    # Test validation
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(input_contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_add_empty_contact(app):
-    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", companyname="", address="",
+    old_contacts = app.contact.get_contact_list()
+    input_contact = Contact(firstname="", middlename="", lastname="", nickname="", companyname="", address="",
                                homenumber="", worknumber="", email1="", email2="",
                                birth_date="//div[@id='content']/form/select[1]//option[1]",
                                birth_month="//div[@id='content']/form/select[2]//option[1]", birth_year="",
                                anniversary_date="//div[@id='content']/form/select[3]//option[1]",
-                               anniversary_month="//div[@id='content']/form/select[4]//option[1]", notes=""))
+                               anniversary_month="//div[@id='content']/form/select[4]//option[1]", notes="")
+    app.contact.create(input_contact)
+    # Test validation
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(input_contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -8,7 +8,8 @@ def test_edit_first_contact(app):
                            birth_month="//div[@id='content']/form/select[2]//option[1]",
                            anniversary_date="//div[@id='content']/form/select[3]//option[1]",
                            anniversary_month="//div[@id='content']/form/select[4]//option[1]"))
-    app.contact.edit_first_contact(Contact(firstname="Отредактирован", middlename="Отредактирович",
+    old_contacts = app.contact.get_contact_list()
+    input_contact = Contact(firstname="Отредактирован", middlename="Отредактирович",
                                          lastname="Отредактированский", nickname="Редактор",
                                          companyname='ОАО "Редакция и Мир"', address="редакторский городок",
                                          homenumber="567-22-04", worknumber="456", email1="glavred@mir.ur",
@@ -16,4 +17,12 @@ def test_edit_first_contact(app):
                                          birth_date="//div[@id='content']/form/select[1]//option[4]",
                                          birth_month="//div[@id='content']/form/select[2]//option[5]", birth_year="",
                                          anniversary_date="//div[@id='content']/form/select[3]//option[6]",
-                                         anniversary_month="//div[@id='content']/form/select[4]//option[7]"))
+                                         anniversary_month="//div[@id='content']/form/select[4]//option[7]")
+    app.contact.edit_first_contact(input_contact)
+    # Test validation
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = input_contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
