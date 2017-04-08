c2f827bea3feb5b2a5e39de3c1c8b5da1e754384
AklerQ <akler.mortiferus@gmail.com>
2017-04-09 00:52:30 +0300

    Добавлен метод для очищения спецсимволов из номера телефона

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,14 @@
+import re
 
 
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.homenumber == contact_from_edit_page.homenumber
-    assert contact_from_home_page.mobilenumber == contact_from_edit_page.mobilenumber
-    assert contact_from_home_page.worknumber == contact_from_edit_page.worknumber
-    assert contact_from_home_page.secondarynumber == contact_from_edit_page.secondarynumber
+    assert contact_from_home_page.homenumber == clear(contact_from_edit_page.homenumber)
+    assert contact_from_home_page.mobilenumber == clear(contact_from_edit_page.mobilenumber)
+    assert contact_from_home_page.worknumber == clear(contact_from_edit_page.worknumber)
+    assert contact_from_home_page.secondarynumber == clear(contact_from_edit_page.secondarynumber)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
