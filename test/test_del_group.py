b5c75415388ef18c79870b5b219151be70fd2fcc
AklerQ <akler.mortiferus@gmail.com>
2017-04-30 01:51:24 +0300

    Добавлены отключаемые проверки UI

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 
 
-def test_delete_random_group(app, db):
+def test_delete_random_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="For delete", header="For delete", footer="For delete"))
     old_groups = db.get_group_list()
@@ -14,4 +14,7 @@ def test_delete_random_group(app, db):
     assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        new_groups = map(app.group.clean, db.get_group_list())
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
