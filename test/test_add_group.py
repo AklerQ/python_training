390287771d8da69eef9a2066f9e74c038adb7909
AklerQ <akler.mortiferus@gmail.com>
2017-04-30 01:10:58 +0300

    Тест добавления группы переделан под работу с БД

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,12 +2,11 @@
 from model.group import Group
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     input_group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(input_group)
     # Test validation
-    assert len(old_groups) + 1 == app.group.count_groups()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(input_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
