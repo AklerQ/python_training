3d5ada7111e3191d62b8a7b6556c9f103548aead
AklerQ <akler.mortiferus@gmail.com>
2017-05-01 22:45:59 +0300

    Содан тест для проверки данных всех контактов

--- fixture/db.py
+++ fixture/db.py
@@ -32,10 +32,13 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, email, email2, email3, address, home, mobile, work, phone2 "
+                           "from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, email, email2, email3, address, home, mobile, work, phone2) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, email=email, email2=email2,
+                                    email3=email3, address=address, homenumber=home, mobilenumber=mobile,
+                                    worknumber=work, secondarynumber=phone2))
         finally:
             cursor.close()
         return list
